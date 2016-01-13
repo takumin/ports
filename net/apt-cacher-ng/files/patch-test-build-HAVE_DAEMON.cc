031c4663f4b9018e5a367ac111222ef385fc481a
takumin <takumiiinn@gmail.com>
2016-01-13 13:17:38 +0900

    fix

--- net/apt-cacher-ng/files/patch-test-build-HAVE_DAEMON.cc
+++
@@ -1,7 +0,0 @@
---- test/build/HAVE_DAEMON.cc.orig	2015-09-21 20:05:55 UTC
-+++ test/build/HAVE_DAEMON.cc
-@@ -1,3 +1,3 @@
- 
--#include <unistd.h>
-+#include <stdlib.h>
- int main() { return daemon(0, 0); }
