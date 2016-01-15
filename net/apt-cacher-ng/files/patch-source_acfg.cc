5990daafd075dbf107808b694c000aa9aa966ed4
takumin <takumiiinn@gmail.com>
2016-01-16 01:58:53 +0900

    fix

--- net/apt-cacher-ng/files/patch-source_acfg.cc
+++ net/apt-cacher-ng/files/patch-source_acfg.cc
@@ -1,4 +1,4 @@
---- source/acfg.cc.orig	2016-01-15 16:13:19 UTC
+--- source/acfg.cc.orig	2015-12-20 14:54:05 UTC
 +++ source/acfg.cc
 @@ -180,7 +180,7 @@ tProperty n2pTbl[] =
  		BARF("Invalid proxy specification, aborting...");
