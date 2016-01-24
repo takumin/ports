983a753dbdc5c22d54956a6fb16577c44152e001
takumin <takumiiinn@gmail.com>
2016-01-24 17:35:44 +0900

    fix

--- net/apt-cacher-ng/files/patch-source_apt-cacher.cc
+++ net/apt-cacher-ng/files/patch-source_apt-cacher.cc
@@ -1,4 +1,4 @@
---- source/apt-cacher.cc.orig	2015-12-20 14:54:05 UTC
+--- source/apt-cacher.cc.orig	2016-01-24 08:32:43 UTC
 +++ source/apt-cacher.cc
 @@ -5,6 +5,7 @@
  #include <stddef.h>
--- net/apt-cacher-ng/files/patch-test_build_HAVE__DAEMON.cc
+++ net/apt-cacher-ng/files/patch-test_build_HAVE__DAEMON.cc
@@ -1,7 +1,7 @@
---- test/build/HAVE_DAEMON.cc.orig	2015-12-20 14:54:05 UTC
+--- test/build/HAVE_DAEMON.cc.orig	2016-01-24 08:33:39 UTC
 +++ test/build/HAVE_DAEMON.cc
-@@ -1,3 +1,2 @@
--
--#include <unistd.h>
+@@ -1,3 +1,4 @@
+ 
+ #include <unistd.h>
 +#include <stdlib.h>
  int main() { return daemon(0, 0); }
